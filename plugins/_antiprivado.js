0285620932ed9c20e2c2127c2742d6f637617a22
Wichi <sebastianbarbaro82@gmail.com>
2025-08-05 16:43:59 -0400

    Actualizar _antiprivado.js

---
+++ plugins/_antiprivado.js
@@ -0,0 +1,9 @@
+
+export async function before(m, { conn, isOwner, isROwner}) {
+  if (m.isGroup) return false; // solo se aplica a chats privados
+  if (!isOwner &&!isROwner) {
+    await conn.updateBlockStatus(m.chat, 'block'); // bloqueo silencioso
+    return true; // evita procesar el mensaje
+}
+  return false;
+}
