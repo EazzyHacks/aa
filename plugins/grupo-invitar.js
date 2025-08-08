ab43b9a8a87c128aefc139699f4bfc0ef674f47a
EazzyHacks <yhair7200@gmail.com>
2025-08-08 12:56:06 +0200

    Update grupo-invitar.js

--- plugins/grupo-invitar.js
+++ plugins/grupo-invitar.js
@@ -13,6 +13,6 @@ handler.tags = ['grupo']
 handler.command = ['invite', 'invitar'] 
 handler.group = true
 //handler.admin = true
-handler.botAdmin = true
+handler.botAdmin = false
 
 export default handler
