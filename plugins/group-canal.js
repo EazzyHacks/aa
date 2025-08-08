2211940dd5cc8e06ccd92e511bc01abdf3616ec8
EazzyHacks <yhair7200@gmail.com>
2025-08-08 11:40:07 +0200

    Update group-canal.js

--- plugins/group-canal.js
+++ plugins/group-canal.js
@@ -9,7 +9,7 @@ let yaNotificados = new Set(
 );
 
 const enviarAvisoCanal = async (conn, notifyChat = null) => {
-  const mensaje = `⭐ *¡Te invitamos a nuestro canal!* 🌟\n\nEste es el canal oficial 📢 de *Barboza*:\n\n👉 https://whatsapp.com/channel/0029VbBeg0d0VycQYKhB060a\n\nSíguelo para estar al tanto de *comandos, novedades y actualizaciones*. ¡Gracias por tu apoyo! 🙌`;
+  const mensaje = `⭐ *¡Te invitamos a nuestro canal!* 🌟\n\nEste es el canal oficial 📢 de *Evolution*:\n\n👉 https://whatsapp.com/channel/0029Vb5oUp43LdQUVViHwc0m\n\nSíguelo para estar al tanto de *comandos, novedades y actualizaciones*. ¡Gracias por tu apoyo! 🙌`;
 
   const chats = Object.entries(conn.chats).filter(([jid, chat]) => jid && chat.isChats);
   let usuarios = [];
