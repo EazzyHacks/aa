f271984ff2ed6bd0891770145c36a74885d6784d
EazzyHacks <yhair7200@gmail.com>
2025-08-08 13:34:17 +0200

    Update main-grupos.js

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -2,16 +2,16 @@ import fetch from 'node-fetch';
 
 let handler = async (m, { conn, usedPrefix, command }) => {
     let grupos = "*Hola!, te invito a unirte a los grupos oficiales del Bot para convivir con la comunidad* ⭐\n\n" +
-                 "1-Barboza\n" +
-                 "*✰* https://chat.whatsapp.com/CBuLXuVZcg9FEfCSHiY6b0" +
+                 "1- Ventas Online\n" +
+                 "*✰* https://chat.whatsapp.com/" +
                  "*─ׄ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׄ*\n\n" +
                  "➠ Enlace anulado? entre aquí! \n\n" +
                  "⭐ Canal :\n" +
-                 "*✰*https://whatsapp.com/channel/0029Vb8kvXUBfxnzYWsbS81I" +
-                 "> By Barboza";
+                 "*✰*https://whatsapp.com/channel/0029Vb5oUp43LdQUVViHwc0m" +
+                 "> By Evolution";
 
     // Asegúrate de definir 'imagen2' correctamente antes de usarlo
-    let imagen2 = 'https://qu.ax/Mvhfa.jpg';
+    let imagen2 = 'https://files.catbox.moe/hrey02.png';
 
     // Define los emojis que quieres usar
     let emojis = '🍁';
