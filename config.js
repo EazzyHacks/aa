6dc98ac5c68e29580cc4629131e9aa7530017625
EazzyHacks <yhair7200@gmail.com>
2025-08-08 09:57:37 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,36 +8,30 @@ import axios from 'axios'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-  global.owner = [
-  [ '172056423456949@lid', 'Barboza OFC', true ],
-  [ '50577688033', 'Goku', true ],
-  [ '584146277368', 'Willzek', true ],
-  [ '174560573964411@lid', 'Rayo OFC', true ],
-  [ '18498640873', "I'm Fz ~", true ],
-  [ '573113406369', 'bajobots', true ],
-  [ '51928626068', "MediaHub",true ],
-  [ '172056423456949', true ]
+ global.owner = [
+  [ '222265178386528@lid', 'Evolution', true ],
+  [ '51957764960', true ]
   ]
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+//─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─
 
 global.mods = []
 global.prems = []
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.packname = `[ sᥲsᥙkᥱ ᑲ᥆𝗍 🌀`
-global.author = 'sᥲsᥙkᥱ ᑲ᥆𝗍 mძ 🌀]'
-global.stickpack = '©sᥲsᥙkᥱ ᑲ᥆𝗍 mძ 🌀'
-global.stickauth = 'ᑲᥡ sᥲsᥙkᥱ ᑲ᥆𝗍'
-global.wm = 'sᥲsᥙkᥱ 🌀'
-global.dev = '© 𝖯᥆𝗐ᥱ𝗋ᥱძ ᑲᥡ 𝖲ᥙᥒ𝖿ᥣᥲ𝗋ᥱ  ☂︎  𝖳ᥱᥲ𝗆'
-global.wait = '🌪️ *Aɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ, sᴏʏ ʟᴇɴᴛ𝙾... ฅ^•ﻌ•^ฅ\n\n> sᥲsᥙkᥱ ᑲ᥆𝗍 mძ 🌀 🌪️*'
-global.botname = '[ sᥲsᥙkᥱ ᑲ᥆𝗍 mძ 🌀 ]'
-global.textbot = `⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥲsᥙkᥱ 🌀`
-global.dev = '• ⍴᥆ᥕᥱrᥱძ ᑲᥡ ᥴ᥆mᥙᥒі𝗍ᥡ sᥲsᥙkᥱ ᑲ᥆𝗍 mძ 🌀'
+global.packname = `【 𝐄𝐯𝐨𝐥𝐮𝐭𝐢𝐨𝐧 𝐁𝐨𝐭 】`
+global.author = '【 @𝐞𝐯𝐨𝐥𝐮𝐭𝐢𝐨𝐧.𝐡𝐚𝐜𝐤 】'
+global.stickpack = '【 𝐄𝐯𝐨𝐥𝐮𝐭𝐢𝐨𝐧 𝐒𝐭𝐢𝐜𝐤𝐞𝐫 】'
+global.stickauth = '【 @𝐞𝐯𝐨𝐥𝐮𝐭𝐢𝐨𝐧.𝐡𝐚𝐜𝐤 】'
+global.wm = '【 𝐄𝐯𝐨𝐥𝐮𝐭𝐢𝐨𝐧 𝐁𝐨𝐭 】'
+global.dev = '【 𝐄𝐯𝐨𝐥𝐮𝐭𝐢𝐨𝐧 𝐁𝐨𝐭 】'
+global.wait = '🌪️ *Aɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ, sᴏʏ ʟᴇɴᴛ𝙾... ฅ^•ﻌ•^ฅ\n\n> 【 𝐄𝐯𝐨𝐥𝐮𝐭𝐢𝐨𝐧 𝐁𝐨𝐭 】*'
+global.botname = '【 𝐄𝐯𝐨𝐥𝐮𝐭𝐢𝐨𝐧 𝐁𝐨𝐭 】'
+global.textbot = `【 𝐄𝐯𝐨𝐥𝐮𝐭𝐢𝐨𝐧 𝐁𝐨𝐭 】`
+global.dev = '【 @𝐞𝐯𝐨𝐥𝐮𝐭𝐢𝐨𝐧.𝐡𝐚𝐜𝐤 】'
 global.listo = '*Aqui tiene ฅ^•ﻌ•^ฅ*'
-global.namechannel = 'sᥲsᥙkᥱ ᑲ᥆𝗍 mძ 🌀'
-global.channel = 'https://whatsapp.com/channel/0029Vaua0ZD3gvWjQaIpSy18'
+global.namechannel = '【 𝐄𝐯𝐨𝐥𝐮𝐭𝐢𝐨𝐧 𝐁𝐨𝐭 】'
+global.channel = 'https://whatsapp.com/channel/0029Vb5oUp43LdQUVViHwc0m'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
@@ -46,9 +40,9 @@ global.miniurl = fs.readFileSync('./storage/img/miniurl.jpg')
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.group = 'https://chat.whatsapp.com/CBuLXuVZcg9FEfCSHiY6b0'
-global.canal = 'https://whatsapp.com/channel/0029Vaua0ZD3gvWjQaIpSy18'
-global.insta = 'https://www.insta.com/https://www.insta.com/sebastian_barboza13'
+global.group = 'https://chat.whatsapp.com/'
+global.canal = 'https://whatsapp.com/channel/0029Vb5oUp43LdQUVViHwc0m'
+global.insta = 'https://www.instagram.com/evolution.hack'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
@@ -56,8 +50,8 @@ global.catalogo = fs.readFileSync('./storage/img/catalogo.png');
 global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 
 global.ch = {
-ch1: '120363419364337473@newsletter',
-ch2: '120363414007802886@newsletter'
+ch1: '120363419947391620@newsletter',
+ch2: '120363419947391620@newsletter'
 }
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
@@ -66,8 +60,8 @@ global.cheerio = cheerio
 global.fs = fs
 global.fetch = fetch
 global.axios = axios
-global.jadi = 'Sesiones/Subbots'
-global.Sesion = 'Sesiones/Principal'
+global.jadi = 'Bots'
+global.Sesion = 'Evolution'
 global.dbname = 'database.json'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
